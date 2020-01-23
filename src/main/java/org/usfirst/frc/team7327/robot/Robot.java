d139f46039573bd98d734a85e6e23267242a16fc
OxnardHighSchoolRoboticsMAIN <OxnardHighSchoolRobotics@gmail.com>
2020-01-23 09:35:16 -0800

    merging with auto

--- src/main/java/org/usfirst/frc/team7327/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team7327/robot/Robot.java
@@ -12,7 +12,7 @@ import org.usfirst.frc.team7327.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj.I2C;
 
 public class Robot extends TimedRobot {
-  public static final Drivetrain Drivetrain = new Drivetrain();
+  public static final Drivetrain m_swerve = new Drivetrain();
   public static final OI oi = new OI();
   public static AHRS nav; 
   public boolean flag = true; 
@@ -21,14 +21,34 @@ public class Robot extends TimedRobot {
     // CameraServer.getInstance().startAutomaticCapture();
     // c0.setClosedLoopControl(true); 
   }
-  @Override public void robotPeriodic() { Drivetrain.updateDashboard();}
-@Override public void teleopInit() { /*Drivetrain.SetElevatorStatus(); Drivetrain.ConfigElevator();*/ }
-  @Override public void autonomousInit() { nav.reset(); }
+  @Override public void robotPeriodic() { m_swerve.updateDashboard();}
+@Override public void teleopInit() { /*m_swerve.SetElevatorStatus(); m_swerve.ConfigElevator();*/ }
+  @Override public void autonomousInit() { 
+    nav.reset();
+    
+    RobotMoveY(1);
+    RobotMoveY(1);
+    RobotMoveX(1);
+    RobotMoveY(4);
+  }
+  public void RobotMoveY(int y){
+    if (y > 0){while (m_swerve.ODOY() > 1){
+      m_swerve.drive (0, 1, 0, true);}
+    }else if (y < 0){while (m_swerve.m_odometry.getPoseMeters().getTranslation().getY() < 1){
+      m_swerve.drive (0, -1, 0, true);}
+    }
+}public void RobotMoveX(int x){
+    if (x > 0){while(m_swerve.ODOX() > 1){
+      m_swerve.drive (1, 0, 0, true); }
+    }else if(x < 0){while (m_swerve.m_odometry.getPoseMeters().getTranslation().getX() < 1){
+      m_swerve.drive (-1, 0, 0, true); 
+    }
+  }
   @Override public void autonomousPeriodic() { Scheduler.getInstance().run(); }
   @Override public void teleopPeriodic() { Scheduler.getInstance().run();
     Drivetrain.updateOdometry();
-    SmartDashboard.putNumber("ODOX", Drivetrain.m_odometry.getPoseMeters().getTranslation().getX());
-    SmartDashboard.putNumber("ODOY", Drivetrain.m_odometry.getPoseMeters().getTranslation().getY());
+    SmartDashboard.putNumber("ODOX", m_swerve.m_odometry.getPoseMeters().getTranslation().getX());
+    SmartDashboard.putNumber("ODOY", m_swerve.m_odometry.getPoseMeters().getTranslation().getY());
     // if(oi.LSClick(oi.Controller1)){
     //   if(flag){ c0.setClosedLoopControl(false); flag = false; }
     //   else{ c0.setClosedLoopControl(true); flag = true; }
