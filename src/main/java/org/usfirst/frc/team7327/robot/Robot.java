0710c3b91631af00005320c8c8d6a690c00a7398
OxnardHighSchoolRoboticsMAIN <OxnardHighSchoolRobotics@gmail.com>
2020-01-25 14:33:10 -0800

    lidar code

--- src/main/java/org/usfirst/frc/team7327/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team7327/robot/Robot.java
@@ -8,26 +8,48 @@ import com.kauailabs.navx.frc.AHRS;
 import org.usfirst.frc.team7327.robot.subsystems.Drivetrain;
 
 import edu.wpi.first.cameraserver.CameraServer;
-//import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.Counter;
 
 public class Robot extends TimedRobot {
+  private Counter m_LIDAR;
   public static final Drivetrain swerve = new Drivetrain();
   public static final OI oi = new OI();
   public static AHRS nav; 
   public boolean flag = true; 
   static double finalAngle, directMag; 
-  //Compressor c0 = new Compressor(0);
+  
+  final double off  = 10; //offset for sensor. test with tape measure
   @Override public void robotInit() { nav = new AHRS(I2C.Port.kMXP); 
     CameraServer.getInstance().startAutomaticCapture();
-    // c0.setClosedLoopControl(true); 
   }
-  @Override public void robotPeriodic() { swerve.updateDashboard();}
-  @Override public void teleopInit() { /*swerve.SetElevatorStatus(); swerve.ConfigElevator();*/ }
+  @Override public void teleopInit() { 
+    
+    m_LIDAR = new Counter(0); //plug the lidar into PWM 0
+    m_LIDAR.setMaxPeriod(1.00); //set the max period that can be measured
+    m_LIDAR.setSemiPeriodMode(true); //Set the counter to period measurement
+    m_LIDAR.reset();
+  /*swerve.SetElevatorStatus(); swerve.ConfigElevator();*/
+  }
+  @Override public void robotPeriodic() { 
+    swerve.updateDashboard();
+    double dist;
+    try {
+      if(m_LIDAR.get() < 1)
+      dist = 0;
+    else
+      dist = (m_LIDAR.getPeriod()*1000000.0/10.0) - off; //convert to distance. sensor is high 10 us for every centimeter. 
+    SmartDashboard.putNumber("Distance", dist); //put the distance on the dashboard
+    } catch (Exception e) {
+      //TODO: handle exception
+    }
+    
+  
+  }
   @Override public void autonomousInit() { 
     swerve.OdoReset();
     nav.reset();
-    Autonomous.Auto3();
+    Autonomous.Auto();
   }
   
   public static void MoveY(double y){
@@ -41,7 +63,23 @@ public class Robot extends TimedRobot {
       }
     }
     SwerveMath.ComputeSwerve(0,0,0,false);
-  }public static void MoveX(double x){
+  }
+  public static void MovetryY(double y){
+    //how can I make the code be able to flip?
+    y = -y; 
+    if (y > 0){
+      while (swerve.ODOY() < y){ SwerveMath.ComputeSwerve(90,.15,0,false); 
+        Drivetrain.updateOdometry(); swerve.updateDashboard();
+      }
+    }else if (y < 0){
+      while (swerve.ODOY() > y){ SwerveMath.ComputeSwerve(90,-.15,0,false); 
+        Drivetrain.updateOdometry(); swerve.updateDashboard();
+      }
+    }
+    SwerveMath.ComputeSwerve(0,0,0,false);
+  }
+  
+  public static void MoveX(double x){
     if (x > 0){
       while(swerve.ODOX() < x){ SwerveMath.ComputeSwerve(0,.15,0,false); 
         Drivetrain.updateOdometry(); swerve.updateDashboard();
@@ -68,17 +106,12 @@ public class Robot extends TimedRobot {
   public static void MoveTo(double x, double y, double angle){
     y = -y; 
     angle = -angle; 
-    finalAngle = 0; 
-    directMag = 0; 
     while(Math.abs(swerve.ODOX()-x)+Math.abs(swerve.ODOY()-y)+(Math.abs(-angle-Robot.NavAngle())/50) > .1){
       try { Robot.swerve.turning.setYaw(angle + Robot.NavAngle());} catch (Exception e) {}
       finalAngle = Math.toDegrees(Math.atan2(-(swerve.ODOY()-y),-(swerve.ODOX()-x)))-Robot.NavAngle(); 
       directMag = Math.hypot(swerve.ODOY()-y,swerve.ODOX()-x);
       SwerveMath.ComputeSwerve(finalAngle, directMag, Robot.swerve.turning.getPIDOutput(), false);
       Drivetrain.updateOdometry(); swerve.updateDashboard();
-      SmartDashboard.putNumber("x", x);
-      SmartDashboard.putNumber("y", y);
-      SmartDashboard.putNumber("angle", angle);
     }
     SwerveMath.ComputeSwerve(finalAngle, 0, 0, false);
   }
@@ -89,6 +122,7 @@ public class Robot extends TimedRobot {
     Drivetrain.updateOdometry();
     SmartDashboard.putNumber("ODOX", Drivetrain.m_odometry.getPoseMeters().getTranslation().getX());
     SmartDashboard.putNumber("ODOY", Drivetrain.m_odometry.getPoseMeters().getTranslation().getY());
+    
     // if(oi.LSClick(oi.Controller1)){
     //   if(flag){ c0.setClosedLoopControl(false); flag = false; }
     //   else{ c0.setClosedLoopControl(true); flag = true; }
