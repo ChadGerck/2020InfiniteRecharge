4772834b9884037a051517a9d0142cc7c29605ef
OxnardCodeTeam <47262396+OxnardCodeTeam@users.noreply.github.com>
2020-02-20 21:30:26 -0800

    Autonomous3

--- src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/main/java/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -89,29 +89,69 @@ public class Autonomous {
     public static void DoubleCorner7(){
         Robot.MoveTo(0,-2.5,67.5);
         Robot.MoveTo(0,-0.7865,0);
-        //intake
+        Drivetrain.setIntakeMotors(0.75, DoubleSolenoid.Value.kForward);
         Robot.MoveTo(0,-0.7865,0);
+        Drivetrain.setIntakeMotors(0,DoubleSolenoid.Value.kReverse);
     }
     public static void point32(){
         Robot.LimeAlign();
         Drivetrain.Shoot(0.5, 0.5);
-
+        Robot.MoveTo(1.7,-2.2,0);
+        Drivetrain.setIntakeMotors(0.75, DoubleSolenoid.Value.kForward);
+        Robot.MoveTo(1.7,-5,0);
+        Robot.MoveTo(-.5,-3.360,101.25);
+        Robot.MoveTo(-0.660,-2.970,101.25);
+        Drivetrain.setIntakeMotors(0,DoubleSolenoid.Value.kReverse);
+        Robot.MoveTo(0,-2.9,0);
+        Robot.MoveTo(0,0,0);
+        Robot.LimeAlign();
+        Drivetrain.Shoot(0.5, 0.5);
     }
-    public static void number9(){
-        //shootball
-        Robot.MoveTo(0,-2.976,0);
-        //intake
-        Robot.MoveTo(0,-0.737,0);
-        //intake
-        Robot.MoveTo(0,-0.737,0);
-        //intake
+    public static void FarLeft(){
+        Drivetrain.setIntakeMotors(0.75, DoubleSolenoid.Value.kForward);
+        Robot.MoveTo(0,-3.4,0);
+        Robot.MoveTo(-.535,-3.4,90);
+        Drivetrain.setIntakeMotors(0,DoubleSolenoid.Value.kReverse);
+        Robot.MoveTo(4.8,0,0);
+        Robot.LimeAlign();
+        Drivetrain.Shoot(0.5, 0.5);
         
-
-
-        
+    }
+    public static void FarRight(){
+        Robot.MoveTo(0,0,327.8);
+        Drivetrain.Shoot(.5,.5);
+        Drivetrain.setIntakeMotors(0.75, DoubleSolenoid.Value.kForward);
+        Robot.MoveTo(0,-5,0);
+        Drivetrain.setIntakeMotors(0,DoubleSolenoid.Value.kReverse);
+        Robot.MoveTo(0,0,327.8);
+        Robot.LimeAlign();
+        Drivetrain.Shoot(0.5, 0.5);
+    }
+    public static void Middle(){
+        Robot.MoveTo(0,-2.6,299.3);
+        Drivetrain.setIntakeMotors(0.75, DoubleSolenoid.Value.kForward);
+        Robot.MoveTo(-.66,-2.87,299.3);
+        Robot.MoveTo(-.5,-3.26,299.3);
+        Drivetrain.setIntakeMotors(0,DoubleSolenoid.Value.kReverse);
+        Robot.MoveTo(0,0,0);
+        Robot.LimeAlign();
+        Drivetrain.Shoot(0.5, 0.5);
+    }
+    public static void Left (){
+        Robot.MoveTo(0,0,67.2);
+        Robot.LimeAlign();
+        Drivetrain.Shoot(0.5, 0.5);
+        Drivetrain.setIntakeMotors(0.75, DoubleSolenoid.Value.kForward);
+        Robot.MoveTo(0,-2.4,353.6);
+        Robot.MoveTo(-.4,-2.6,353.6);
+        Robot.MoveTo(-.8,-2.8,353.6);
+        Drivetrain.setIntakeMotors(0,DoubleSolenoid.Value.kReverse);
+        Robot.MoveTo(1.3,0,0);
+        Robot.LimeAlign();
+        Drivetrain.Shoot(0.5, 0.5); 
     }
 
-    }
+}
     
 
 
