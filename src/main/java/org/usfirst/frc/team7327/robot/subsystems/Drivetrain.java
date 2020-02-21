1457ca646411df49fc4ba47c5f8ad35d2ed38488
FRC7326 <58712519+FRC7326@users.noreply.github.com>
2020-02-21 10:47:51 -0800

    dpad

--- src/main/java/org/usfirst/frc/team7327/robot/subsystems/Drivetrain.java
+++ src/main/java/org/usfirst/frc/team7327/robot/subsystems/Drivetrain.java
@@ -11,6 +11,8 @@ import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.wpilibj.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 // import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 // import com.revrobotics.CANSparkMax;
@@ -43,12 +45,12 @@ public class Drivetrain extends Subsystem {
   
   //public static ElevatorModule Elevator;
   //public static VictorSPX IntakeMotor, FunnelMotor, ControlPanelMotor;
-  //public static TalonFX ShooterMotor1, ShooterMotor2;
+  public static TalonFX ShooterMotor1, ShooterMotor2;
   //public static CANSparkMax BallHandlerMotor;
   //public static DoubleSolenoid Extendor; 
-  // public Drivetrain(){
-  //   ShooterMotor1 = new TalonFX(9);
-  //   ShooterMotor2 = new TalonFX(10);
+   public Drivetrain(){
+     ShooterMotor1 = new TalonFX(9);
+     ShooterMotor2 = new TalonFX(10);
   //   IntakeMotor       = new VictorSPX(11);
   //   FunnelMotor       = new VictorSPX(12);
   //   ControlPanelMotor = new VictorSPX(13);
@@ -56,7 +58,7 @@ public class Drivetrain extends Subsystem {
   //   Elevator  = new ElevatorModule(15,16); 
   //   turning = new TurnModule(); 
   //   Extendor = new DoubleSolenoid(0,2, 5);
-  // }
+   }
   @Override public void initDefaultCommand() { setDefaultCommand(new Drive()); }
   public static void setModule(String loc,double degrees,double power){
     switch(loc){case "FL":moduleFL.set(degrees,power);break; case "FR":moduleFR.set(degrees,power);break;
@@ -86,6 +88,9 @@ public class Drivetrain extends Subsystem {
   //   ShooterMotor1.set(ControlMode.PercentOutput, shooterpower); ShooterMotor2.set(ControlMode.PercentOutput, -shooterpower);
   //   FunnelMotor.set(ControlMode.PercentOutput,handlepower); BallHandlerMotor.set(handlepower);
   // }
+  
+  public static void TopSpin(double shooterpower){ShooterMotor1.set(ControlMode.PercentOutput, shooterpower); }
+  public static void BotSpin(double shooterpower){ShooterMotor2.set(ControlMode.PercentOutput, -shooterpower);}
   // public static void ControlPanel(double power){ ControlPanelMotor.set(ControlMode.PercentOutput, power); }
   // public static void setRawElevator(double speed){ Elevator.setRawElev(speed); }
 	// public void setElevatorPosition(double position){ Elevator.setPosition(position); }
