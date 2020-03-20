1a4235b272ab5e65e7336733e283d1005ea31b1c
FRC7326 <58712519+FRC7326@users.noreply.github.com>
2020-03-19 23:58:45 -0700

    Rommels Quarintine Push
    CHAD IS TEACHING ME DURING THE TIME IM NOT SUPPOSED TO BE TAUGHT

--- src/main/java/org/usfirst/frc/team7327/robot/ShootModule.java
+++ src/main/java/org/usfirst/frc/team7327/robot/ShootModule.java
@@ -9,16 +9,16 @@ public class ShootModule{
     private double error, diffError, lastError, testPIDOutput; 
     private int topVel; 
     private volatile double PIDOutput = 0;
-    static final double kP = .0002049999, kD = 0; 
+    static final double kP = .001, kD = 0; 
     public ShootModule() {
     	lastError = getError(); 
     	ShootPID = new Notifier(() ->  {
             error = getError(); 
             SmartDashboard.putNumber("errorShoot", error); 
     		diffError = lastError - error; 
-            testPIDOutput = kP * error + kD * diffError; 
-            testPIDOutput = Math.min(testPIDOutput, 1);
-            PIDOutput = Math.max(testPIDOutput, -1); 
+            testPIDOutput = kP * error + kD * diffError+.315;
+            testPIDOutput = Math.min(testPIDOutput, .5);
+            PIDOutput = Math.max(testPIDOutput, -.5); 
             Drivetrain.Shoot(PIDOutput);
             lastError = error; 
             SmartDashboard.putNumber("PIDOutput: ", PIDOutput); 
